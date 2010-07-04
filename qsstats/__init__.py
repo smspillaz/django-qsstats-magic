118c8b16b79c2d0104ea8e42889e953316d68c0b
Mikhail Korobov <kmike84@gmail.com>
2010-07-05 03:26:20 +0600

    Boilerplate is somehow reduced.

--- qsstats/__init__.py
+++ qsstats/__init__.py
@@ -41,47 +41,25 @@ class QuerySetStats(object):
 
     def for_day(self, dt, date_field=None, aggregate_field=None, aggregate_class=None):
         date_field = date_field or self.date_field
-        aggregate_class = aggregate_class or self.aggregate_class
-        aggregate_field = aggregate_field or self.aggregate_field
-
-        self.check_date_field(date_field)
-        self.check_qs()
-
         kwargs = {
             '%s__year' % date_field : dt.year,
             '%s__month' % date_field : dt.month,
             '%s__day' % date_field : dt.day,
         }
-        agg = self.qs.filter(**kwargs).aggregate(agg=aggregate_class(aggregate_field))
-        return agg['agg']
+        return self._aggregate(date_field, aggregate_field, aggregate_class, kwargs)
 
     def this_day(self, date_field=None, aggregate_field=None, aggregate_class=None):
         return self.for_day(self.today, date_field, aggregate_field, aggregate_class)
 
     def for_week(self, dt, date_field=None, aggregate_field=None, aggregate_class=None):
-        date_field = date_field or self.date_field
-        aggregate_class = aggregate_class or self.aggregate_class
-        aggregate_field = aggregate_field or self.aggregate_field
-
-        self.check_date_field(date_field)
-        self.check_qs()
-
         first_day = dt - relativedelta(weekday=MO(-1))
         last_day = first_day + datetime.timedelta(days=7)
-
         return self.get_aggregate(first_day, last_day, date_field, aggregate_field, aggregate_class)
 
     def this_week(self, date_field=None, aggregate_field=None, aggregate_class=None):
         return self.for_week(self.today, date_field, aggregate_class)
 
     def for_month(self, dt, date_field=None, aggregate_field=None, aggregate_class=None):
-        date_field = date_field or self.date_field
-        aggregate_class = aggregate_class or self.aggregate_class
-        aggregate_field = aggregate_field or self.aggregate_field
-
-        self.check_date_field(date_field)
-        self.check_qs()
-
         first_day = datetime.date(year=dt.year, month=dt.month, day=1)
         last_day = first_day + relativedelta(day=31)
         return self.get_aggregate(first_day, last_day, date_field, aggregate_field, aggregate_class)
@@ -90,13 +68,6 @@ class QuerySetStats(object):
         return self.for_month(self.today, date_field, aggregate_class)
 
     def for_year(self, dt, date_field=None, aggregate_field=None, aggregate_class=None):
-        date_field = date_field or self.date_field
-        aggregate_class = aggregate_class or self.aggregate_class
-        aggregate_field = aggregate_field or self.aggregate_field
-
-        self.check_date_field(date_field)
-        self.check_qs()
-
         first_day = datetime.date(year=dt.year, month=1, day=1)
         last_day = datetime.date(year=dt.year, month=12, day=31)
         return self.get_aggregate(first_day, last_day, date_field, aggregate_field, aggregate_class)
@@ -114,8 +85,11 @@ class QuerySetStats(object):
         aggregate_class = aggregate_class or self.aggregate_class
         aggregate_field = aggregate_field or self.aggregate_field
 
-        self.check_date_field(date_field)
-        self.check_qs()
+        if not date_field:
+            raise DateFieldMissing("Please provide a date_field.")
+
+        if not self.qs:
+            raise QuerySetMissing("Please provide a queryset.")
 
         stat_list = []
         dt = start_date
@@ -141,34 +115,32 @@ class QuerySetStats(object):
         return self.pivot(datetime.datetime.now(), 'gte', date_field, aggregate_field, aggregate_class)
 
     def pivot(self, dt, operator=None, date_field=None, aggregate_field=None, aggregate_class=None):
-        date_field = date_field or self.date_field
-        aggregate_class = aggregate_class or self.aggregate_class
-        aggregate_field = aggregate_field or self.aggregate_field
         operator = operator or self.operator
-
-        self.check_date_field(date_field)
-        self.check_qs()
         if operator not in ['lt', 'lte', 'gt', 'gte']:
             raise InvalidOperator("Please provide a valid operator.")
 
-        kwargs = {'%s__%s' % (date_field, operator) : dt}
-
-        total = self.qs.filter(**kwargs).aggregate(total=aggregate_class(aggregate_field))
-        return total['total']
+        kwargs = {'%s__%s' % (date_field or self.date_field, operator) : dt}
+        return self._aggregate(date_field, aggregate_field, aggregate_class, kwargs)
 
     # Utility functions
     def update_today(self):
         self.today = datetime.date.today()
 
-    def get_aggregate(self, first_day, last_day, date_field, aggregate_field, aggregate_class):
-        kwargs = {'%s__range' % date_field : (first_day, last_day)}
-        agg = self.qs.filter(**kwargs).aggregate(agg=aggregate_class(aggregate_field))
-        return agg['agg']
+    def _aggregate(self, date_field=None, aggregate_field=None, aggregate_class=None, filter=None):
+        date_field = date_field or self.date_field
+        aggregate_field = aggregate_field or self.aggregate_field
+        aggregate_class = aggregate_class or self.aggregate_class
 
-    def check_date_field(self, date_field):
         if not date_field:
             raise DateFieldMissing("Please provide a date_field.")
 
-    def check_qs(self):
         if not self.qs:
             raise QuerySetMissing("Please provide a queryset.")
+
+        agg = self.qs.filter(**filter).aggregate(agg=aggregate_class(aggregate_field))
+        return agg['agg']
+
+    def get_aggregate(self, first_day, last_day, date_field=None, aggregate_field=None, aggregate_class=None):
+        date_field = date_field or self.date_field
+        kwargs = {'%s__range' % date_field : (first_day, last_day)}
+        return self._aggregate(date_field, aggregate_field, aggregate_class, kwargs)
